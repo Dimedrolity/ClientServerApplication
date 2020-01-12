70c32076d86518e7da08415d119758e07fb4717c
DRed <dima.redkin.1999@gmail.com>
2020-01-12 16:35:50 +0500

    added new class Library ByteStreamUtils to remove duplicate code from Client and Server. In ByteStreamUtils exists class Streamer which works with bytes and stream (common between client and server)

---
+++ Streamer/Extensions.cs
@@ -0,0 +1,18 @@
+﻿﻿namespace Streamer
+{
+    public static class Extensions
+    {
+        public static int ToIntLittleEndian(this byte[] bytes)
+        {
+            var number = 0;
+            var length = bytes.Length;
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                number = bytes[i] | number << 8;
+            }
+
+            return number;
+        }
+    }
+}
---
+++ Streamer/Streamer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Streamer
+{
+    public class Streamer
+    {
+        private readonly NetworkStream stream;
+        
+        private const int BytesCountForMessageLength = 4;
+        
+        public Streamer(NetworkStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public void Write(string message)
+        {
+            byte[] lengthAndMessage = GetBytesOfLengthAndMessage(message);
+            stream.Write(lengthAndMessage, 0, lengthAndMessage.Length);
+        }
+        
+        private byte[] GetBytesOfLengthAndMessage(string message)
+        {
+            var sentMessage = Encoding.UTF8.GetBytes(message);
+            var sentMessageLength = BitConverter.GetBytes(sentMessage.Length);
+            return sentMessageLength.Concat(sentMessage).ToArray();
+        }
+
+        public string ReadMessage()
+        {
+            var messageBuffer = CreateBuffer();
+            ReadMessageBytesTo(messageBuffer);
+            var response = Encoding.UTF8.GetString(messageBuffer);
+
+            return response;
+        }
+
+        private byte[] CreateBuffer()
+        {
+            var receivedMessageLength = new byte[BytesCountForMessageLength];
+            stream.Read(receivedMessageLength, 0, BytesCountForMessageLength);
+            int messageLength = receivedMessageLength.ToIntLittleEndian();
+            var receivedMessage = new byte[messageLength];
+            return receivedMessage;
+        }
+        
+        private void ReadMessageBytesTo(byte[] receivedMessage)
+        {
+            var bytesCountToRead = receivedMessage.Length;
+
+            var readBytes = 0;
+            while (bytesCountToRead > 0)
+            {
+                readBytes += stream.Read(receivedMessage, readBytes, bytesCountToRead);
+                bytesCountToRead = receivedMessage.Length - readBytes;
+            }
+        }
+        
+        public void CloseStream()
+        {
+            stream.Close();
+        }
+    }
+}
